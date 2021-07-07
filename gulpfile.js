94882a39cdfb93b1b423e91bfecede10b4a5cf4b
Liam.Li <398389303@qq.com>
2021-07-07 22:51:01 +0800

    modify

--- gulpfile.js
+++ gulpfile.js
@@ -1,39 +1,49 @@
 var gulp = require("gulp");
 var sass = require("gulp-sass")(require("sass"));
+const babel = require("gulp-babel");
 var path = require("path");
-var del = require('del')
+var del = require("del");
 
-const rootPath = process.cwd()
+const rootPath = process.cwd();
 const basePath = path.resolve(rootPath, "components");
 const sassPath = path.resolve(basePath, "**/*.scss");
+const tsCodePath = path.resolve(basePath, "**/*.tsx");
 const buildPath_ES = path.resolve(rootPath, "es");
 const buildPath_LIB = path.resolve(rootPath, "lib");
 
-
 const clean = (cb) => {
- return  del([
-    buildPath_ES,
-    buildPath_LIB
-  ],cb)
-}
+  return del([buildPath_ES, buildPath_LIB], cb);
+};
 
-exports.clean = clean 
-
-function buildStyles() {
-  
+const scss = () => {
   return gulp
     .src(sassPath)
     .pipe(sass().on("error", sass.logError))
     .pipe(gulp.dest(buildPath_ES))
     .pipe(gulp.dest(buildPath_LIB));
-}
+};
 
-exports.buildStyles = buildStyles;
-
-
-exports.watch = function () {
-  gulp.watch(sassPath, ["sass"]);
+const tsbuild = () => {
+  return gulp
+    .src(tsCodePath)
+    .pipe(
+      babel({
+        presets: ["@babel/preset-react","@babel/preset-typescript"],
+      })
+    )
+    .pipe(gulp.dest(buildPath_ES))
+    .pipe(gulp.dest(buildPath_LIB));
 };
 
 
-exports.default = gulp.series(clean, buildStyles);
+const watch = function () {
+  gulp.watch(sassPath, ["scss"]);
+};
+ 
+
+exports.scss = scss
+exports.clean = clean
+exports.tsbuild = tsbuild
+exports.watch = watch
+
+exports.default = gulp.series(clean, tsbuild, scss);
