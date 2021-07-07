2d06179f85b95a9b8536751041897384c8db9218
Liam.Li <398389303@qq.com>
2021-07-08 00:13:14 +0800

    test

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,11 @@
 var gulp = require("gulp");
 var sass = require("gulp-sass")(require("sass"));
 const babel = require("gulp-babel");
+var merge = require('merge2');
 var path = require("path");
 var del = require("del");
+const ts = require('gulp-typescript')
+
 
 const rootPath = process.cwd();
 const basePath = path.resolve(rootPath, "components");
@@ -10,6 +13,11 @@ const sassPath = path.resolve(basePath, "**/*.scss");
 const tsCodePath = path.resolve(basePath, "**/*.tsx");
 const buildPath_ES = path.resolve(rootPath, "es");
 const buildPath_LIB = path.resolve(rootPath, "lib");
+const tsConfig = require('./tsconfig.json')
+console.log(tsConfig)
+// var tsProject = ts.createProject('');
+
+const tsDefaultReporter = ts.reporter.defaultReporter();
 
 const clean = (cb) => {
   return del([buildPath_ES, buildPath_LIB], cb);
@@ -27,13 +35,25 @@ const tsbuild = () => {
   return gulp
     .src(tsCodePath)
     .pipe(
-      babel({
-        presets: ["@babel/preset-react","@babel/preset-typescript"],
-      })
+      ts(tsConfig)
     )
+    .on('error', () => { /* Ignore compiler errors */}) 
     .pipe(gulp.dest(buildPath_ES))
-    .pipe(gulp.dest(buildPath_LIB));
+    
 };
+
+
+// const tsbuild = () => {
+//   return gulp
+//     .src(tsCodePath)
+//     .pipe(
+//       babel({
+//         presets: ["@babel/preset-react","@babel/preset-typescript"],
+//       })
+//     )
+//     .pipe(gulp.dest(buildPath_ES))
+//     .pipe(gulp.dest(buildPath_LIB));
+// };
 
 
 const watch = function () {
