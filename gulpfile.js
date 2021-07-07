feef17334feb44e73199a9d4f9e8197f8aa65666
Liam.Li <398389303@qq.com>
2021-07-07 22:15:31 +0800

    gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,39 @@
 var gulp = require("gulp");
 var sass = require("gulp-sass")(require("sass"));
 var path = require("path");
+var del = require('del')
 
-const basePath = path.resolve(process.cwd(), "components");
+const rootPath = process.cwd()
+const basePath = path.resolve(rootPath, "components");
 const sassPath = path.resolve(basePath, "**/*.scss");
-const buildPath_ES = path.resolve(basePath, "es");
+const buildPath_ES = path.resolve(rootPath, "es");
+const buildPath_LIB = path.resolve(rootPath, "lib");
+
+
+const clean = (cb) => {
+ return  del([
+    buildPath_ES,
+    buildPath_LIB
+  ],cb)
+}
+
+exports.clean = clean 
 
 function buildStyles() {
-  console.log('buildStyles')
+  
   return gulp
     .src(sassPath)
     .pipe(sass().on("error", sass.logError))
-    .pipe(gulp.dest(buildPath_ES));
+    .pipe(gulp.dest(buildPath_ES))
+    .pipe(gulp.dest(buildPath_LIB));
 }
 
 exports.buildStyles = buildStyles;
+
+
 exports.watch = function () {
   gulp.watch(sassPath, ["sass"]);
 };
+
+
+exports.default = gulp.series(clean, buildStyles);
